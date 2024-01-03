116ece5b6dafed131814004cebaf72b5a16abe88
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-01-03 13:34:59 +0200

    fix: revert changes for box padding

--- packages/ui/src/components/box/styles.ts
+++ packages/ui/src/components/box/styles.ts
@@ -4,6 +4,7 @@ const useStyles = makeStyles()((theme) => ({
   root: {
     '&&': {
       overflow: 'auto',
+      padding: theme.spacing(2),
       borderRadius: theme.shape.borderRadius,
       background: theme.palette.background.paper,
     },
