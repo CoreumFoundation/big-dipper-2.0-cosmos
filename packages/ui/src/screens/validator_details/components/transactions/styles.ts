06ecb1d7f2c038467bee526b5b8d022ba2e840e8
Oleksandr Khlopiachyi <akhlopiachyi@gmail.com>
2024-11-11 19:11:49 +0200

    fix: update styles for validator's details page

--- packages/ui/src/screens/validator_details/components/transactions/styles.ts
+++ packages/ui/src/screens/validator_details/components/transactions/styles.ts
@@ -3,6 +3,7 @@ import { makeStyles } from 'tss-react/mui';
 const useStyles = makeStyles()((theme) => ({
   root: {
     '& .MuiTypography-h2': {
+      fontSize: theme.spacing(2.5),
       marginBottom: theme.spacing(2),
     },
   },
